59ecfcc077defd1cfd2a475e511e30c3b8e06000
Jean Matheus Souto <jeanmatheussouto@gmail.com>
2015-11-30 00:14:01 -0200

    end of week 2

--- image_share.js
+++ image_share.js
@@ -1,7 +1,9 @@
 Images = new Mongo.Collection("images");
 
 if (Meteor.isClient) {
-  Template.images.helpers({images: Images.find()});
+  Template.images.helpers({
+    images: Images.find({}, {sort: {createdAt: -1, rating: -1}})
+  });
 
   Template.images.events({
     'click .js-image': function(event){
@@ -13,6 +15,31 @@ if (Meteor.isClient) {
       $("#" + image_id).hide('slow', function(){
         Images.remove({"_id": image_id});
       });
+    },
+    'click .js-rate-image': function(event){
+      var rate = $(event.currentTarget).data('userrating')
+      var image_id = this.id;
+      Images.update({_id: image_id}, {$set: {rating: rate}});
+    },
+    'click .js-show-image-modal': function(event){
+      $("#image_add_form").modal('show');
+    }
+  });
+
+  Template.image_add_form.events({
+    'submit .js-add-image': function(event){
+      var img_src = event.target.img_src.value;
+      var img_alt = event.target.img_alt.value;
+
+      Images.insert({
+        img_src: img_src,
+        img_alt: img_alt,
+        createdAt: new Date()
+      });
+
+      $("#image_add_form").modal('hide');
+
+      return false;
     }
   });
 }
